3aa813f11b53ce71ae2810c312715a488b1b9c8c
quentinberry <34132543+quentinberry@users.noreply.github.com>
2024-06-16 13:34:18 +0200

    init

---
+++ components/Button.tsx
@@ -0,0 +1,55 @@
+"use client";
+import React, { Suspense, useEffect, useMemo, useState } from "react";
+import { Button } from "./ui/button";
+import Loading from "@/app/loading";
+import { alphabet } from "@/lib/utils";
+
+interface ButtonAlphabetProps {
+  setStoredName: (name: string) => void;
+}
+
+const ButtonAlphabet: React.FC<ButtonAlphabetProps> = ({ setStoredName }) => {
+  const [name, setName] = useState<string>("");
+  const [realShuffledArray, setRealShuffledArray] = useState<string[]>([]);
+
+  // setStoredName everytime name updates. This is passed to parent component
+  useEffect(() => {
+    setStoredName(name);
+  }, [setStoredName, name]);
+
+  const shuffleArray = () => {
+    setRealShuffledArray([...alphabet].sort(() => 0.5 - Math.random()));
+  };
+
+  // shuffles Array on initial load
+  useEffect(() => {
+    shuffleArray();
+  }, []);
+
+  function appendToName(index: number): void {
+    setName(name.concat(realShuffledArray[index]));
+    shuffleArray();
+  }
+
+  function deleteFromName(): void {
+    setName("");
+  }
+
+  return (
+    <>
+      {realShuffledArray.map((letter: string, i: number) => (
+        <Button
+          key={i}
+          variant={"destructive"}
+          size={"sm"}
+          onClick={() => appendToName(i)}
+        >
+          {letter}
+        </Button>
+      ))}
+      <Button onClick={() => deleteFromName()}>←</Button>
+    </>
+  );
+};
+
+export default ButtonAlphabet;
